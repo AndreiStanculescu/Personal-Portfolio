774bbd6f8f51af33cc2fb4a2f24542572c8fe62a
AndreiStanculescu <andrei.florin.stanculescu@gmail.com>
2026-03-08 21:18:50 +0200

    Initial commit - setup CV website

--- index.php
+++ index.php
@@ -1,85 +1,12 @@
-<?php
-require_once __DIR__ . '/../config.php';
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
-?>
 <!DOCTYPE html>
-<html lang="<?= $lang ?>">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- import font logo -->
-    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;600&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" href="<?= $logo ?>">
-
-    <title><?= $t['nume_aplicatie'] ?></title>
-
-
-
-    <style>
-        :root {
-            --body-color: <?= $culoareBody ?>;
-            --h1-color: <?= $culoareH1 ?>;
-            --label-color: <?= $culoareLabel ?>;
-            --header-color: <?= $culoareHeader ?>;
-            --footer-color: <?= $culoareFooter ?>;
-        }
-    </style>
-
-    <!-- CSS -->
-    <!-- <link rel="stylesheet" href="css/style-index.css"> -->
-    <link rel="stylesheet" href="css/style-index.css?v=<?php echo filemtime('css/style-index.css'); ?>">
-
+    <title>Document</title>
 </head>
-
 <body>
-    <?php include 'header.php'; ?>
-
-    <section class="hero">
-        <!-- <img src="../assets/poza_acasa2.png" class="hero-img"> -->
-
-        <div class="logo-a">
-            <span><?= $t['nume_aplicatie'] ?></span>
-        </div>
-
-        <!-- Pictograma -->
-        <img src="assets/logo.png" alt="Logo mic" class="hero-logo-mobile">
-
-        <section class="cards-section">
-            <!-- <h2 class="cards-title">L'académie Chic</h2> -->
-            <div class="cards-container">
-
-                <a href="franceza_medicala.php" class="card">
-                    <h3><?= $t['franceza-medicala'] ?></h3>
-                </a>
-
-                <a href="romana_medicala.php" class="card">
-                    <h3><?= $t['romana-medicala'] ?></h3>
-                </a>
-
-                <a href="franceza_business.php" class="card">
-                    <h3><?= $t['franceza-business'] ?></h3>
-                </a>
-
-                <a href="romana_business.php" class="card">
-                    <h3><?= $t['romana-business'] ?></h3>
-                </a>
-
-                <a href="franceza_generala.php" class="card">
-                    <h3><?= $t['franceza-generala'] ?></h3>
-                </a>
-
-                <a href="romana_generala.php" class="card">
-                    <h3><?= $t['romana-generala'] ?></h3>
-                </a>
-
-            </div>
-        </section>
-    </section>
-
-    <?php include 'footer.php'; ?>
+    <label>Andrei dev</label>
+    
 </body>
-
 </html>
